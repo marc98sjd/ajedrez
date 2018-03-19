4694f95c8c92c810d4f3297226b031878838ec84
marc98_sjd <marc98_sjd@hotmail.es>
2018-03-19 16:24:56 +0100

    se actualiza?

---
+++ escacs/database/migrations/2018_03_12_164652_create_partidas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePartidasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('partidas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('finalizada')->default(0);
+            $table->foreign('jugador1')->references('id')
+            ->on('users')->onDelete('cascade');
+            $table->foreign('jugador2')->references('id')
+            ->on('users')->onDelete('cascade');
+            $table->integer('turno')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('partidas');
+    }
+}
